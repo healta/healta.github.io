b662ef6532fadc7039a74753aa62d22dc52b76d4
healta <m_95@abv.bg>
2022-07-20 22:32:12 +0300

    rip CPU

--- canvas.js
+++ canvas.js
@@ -1,17 +1,15 @@
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
-canvas.width = 800;
-canvas.height = 800;
- 
+let widthCanvas = 8000;
+let heightCanvas = 8000;
+
 let y = []
 let array = []
 let yy = []
 let bufferArray=[]
-let zz =[]
 
-
-for (let i = 0; i<80; i++){
-    y = Array.from({length:80}, () => Math.round(Math.random()))
+for (let i = 0; i<800; i++){
+    y = Array.from({length:800}, () => Math.round(Math.random()))
     array.push(y)
 }
 
@@ -19,139 +17,267 @@ for (let i = 0; i<80; i++){
 
 const arrayZero =() =>{
     let arr1 = []
-    for (let i = 0; i<80; i++){
-        yy = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    for (let i = 0; i<800; i++){
+        yy = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
         arr1.push(yy)
     }
     return arr1
 }
 
 
-
-function animate(){
-    ctx.clearRect(0,0,canvas.width, canvas.height);
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "white";
-    
-    
-    
-    for (let i = 0; i<80; i++){
-        for (let ii=0;ii<80;ii++){
-            try{
-                if (array[i][ii] == 1){
-                 ctx.fillRect(i*10,ii*10,10,10)
-                }
-        }catch(e){}
-        }
-        
-        }
-    
-
-    bufferArray = arrayZero();
-    
-    
-    for (let i=0; i<80; i++){
-        for (let ii=0;ii<80;ii++){
-            let neighbour=0;
-            try{
-                if (array[i+1][ii+1]==1){
-                    neighbour ++;
-                }
-            }catch (e){}
-            try{
-                if (array[i-1][ii-1]==1){
-                    neighbour ++;
-                }
-            }catch (e){}
-            try{
-                if (array[i+1][ii-1]==1){
-                    neighbour ++;
-                }
-            }catch (e){}
-            try{
-                if (array[i-1][ii+1]==1){
-                    neighbour ++;
-                }
-            }catch (e){}
-            try{
-                if (array[i][ii+1]==1){
-                    neighbour ++;
-                }
-            }catch (e){}
-            try{
-                if (array[i][ii-1]==1){
-                    neighbour ++;
-                }
-            }catch (e){}
-            try{
-                if (array[i+1][ii]==1){
-                    neighbour ++;
-                }
-            }catch (e){}
-            try{
-                if (array[i-1][ii]==1){
-                    neighbour ++;
-                }
-            }catch (e){}
-    
-    
-    
-            if (array[i][ii] == 1 && neighbour == 0) {
-                bufferArray[i][ii]=0;
-            }else if (array[i][ii] == 1 && neighbour == 1){
-                bufferArray[i][ii]=0;
-            }else if (array[i][ii] == 1 && neighbour == 2){
-                bufferArray[i][ii]=1;
-            }else if (array[i][ii] == 1 && neighbour == 3){
-                bufferArray[i][ii]=1;
-            }else if (array[i][ii] == 1 && neighbour ==4){
-                bufferArray[i][ii]=0;
-            }else if (array[i][ii] == 1 && neighbour == 5){
-                bufferArray[i][ii]=0;
-            }else if (array[i][ii] == 1 && neighbour ==6){
-                bufferArray[i][ii]=0;
-            }else if (array[i][ii] == 1 && neighbour ==7){
-                bufferArray[i][ii]=0;
-            }else if (array[i][ii] == 1 && neighbour ==8){
-                bufferArray[i][ii]=0;
-            }else if (array[i][ii] == 0 && neighbour ==0){
-                bufferArray[i][ii]=0;
-            }else if (array[i][ii] == 0 && neighbour ==1){
-                bufferArray[i][ii]=0;
-            }else if (array[i][ii] == 0 && neighbour ==2){
-                bufferArray[i][ii]=0;
-            }else if (array[i][ii] == 0 && neighbour ==3){
-                bufferArray[i][ii]=1;
-            }else if (array[i][ii] == 0 && neighbour ==4){
-                bufferArray[i][ii]=0;
-            }else if (array[i][ii] == 0 && neighbour ==5){
-                bufferArray[i][ii]=0;
-            }else if (array[i][ii] == 0 && neighbour ==6){
-                bufferArray[i][ii]=0;
-            }else if (array[i][ii] == 0 && neighbour ==7){
-                bufferArray[i][ii]=0;
-            }else if (array[i][ii] == 0 && neighbour ==8){
-                bufferArray[i][ii]=0;
-            }
-        }
-    }
-        
-
-    array = bufferArray
+ let cameraOffset = { x: window.innerWidth/10, y: window.innerHeight/10 }
+ let cameraZoom = 1.2
+ let MAX_ZOOM = 5
+ let MIN_ZOOM = 0.1
+ let SCROLL_SENSITIVITY = 0.0005
+ 
+ function draw()
+ {
+     canvas.width = window.innerWidth
+     canvas.height = window.innerHeight
+     
+     // Translate to the canvas centre before zooming - so you'll always zoom on what you're looking directly at
+     ctx.translate( window.innerWidth / 2, window.innerHeight / 2 )
+     ctx.scale(cameraZoom, cameraZoom)
+     ctx.translate( -window.innerWidth / 2 + cameraOffset.x, -window.innerHeight / 2 + cameraOffset.y )
 
 
-    
-    
-    bufferArray = arrayZero();
-    
-    
-}
+     ctx.clearRect(0,0,widthCanvas, heightCanvas);
+     ctx.fillStyle = "black";
+     ctx.fillRect(0, 0, widthCanvas, heightCanvas);
+     ctx.fillStyle = "white";
+     
+     
+     
+     for (let i = 0; i<800; i++){
+         for (let ii=0;ii<800;ii++){
+             try{
+                 if (array[i][ii] == 1){
+                  ctx.fillRect(i*10,ii*10,10,10)
+                 }
+         }catch(e){}
+         }
+         
+         }
+     
+ 
+     bufferArray = arrayZero();
+     
+     
+     for (let i=0; i<800; i++){
+         for (let ii=0;ii<800;ii++){
+             let neighbour=0;
+             try{
+                 if (array[i+1][ii+1]==1){
+                     neighbour ++;
+                 }
+             }catch (e){}
+             try{
+                 if (array[i-1][ii-1]==1){
+                     neighbour ++;
+                 }
+             }catch (e){}
+             try{
+                 if (array[i+1][ii-1]==1){
+                     neighbour ++;
+                 }
+             }catch (e){}
+             try{
+                 if (array[i-1][ii+1]==1){
+                     neighbour ++;
+                 }
+             }catch (e){}
+             try{
+                 if (array[i][ii+1]==1){
+                     neighbour ++;
+                 }
+             }catch (e){}
+             try{
+                 if (array[i][ii-1]==1){
+                     neighbour ++;
+                 }
+             }catch (e){}
+             try{
+                 if (array[i+1][ii]==1){
+                     neighbour ++;
+                 }
+             }catch (e){}
+             try{
+                 if (array[i-1][ii]==1){
+                     neighbour ++;
+                 }
+             }catch (e){}
+     
+     
+     
+             if (array[i][ii] == 1 && neighbour == 0) {
+                 bufferArray[i][ii]=0;
+             }else if (array[i][ii] == 1 && neighbour == 1){
+                 bufferArray[i][ii]=0;
+             }else if (array[i][ii] == 1 && neighbour == 2){
+                 bufferArray[i][ii]=1;
+             }else if (array[i][ii] == 1 && neighbour == 3){
+                 bufferArray[i][ii]=1;
+             }else if (array[i][ii] == 1 && neighbour ==4){
+                 bufferArray[i][ii]=0;
+             }else if (array[i][ii] == 1 && neighbour == 5){
+                 bufferArray[i][ii]=0;
+             }else if (array[i][ii] == 1 && neighbour ==6){
+                 bufferArray[i][ii]=0;
+             }else if (array[i][ii] == 1 && neighbour ==7){
+                 bufferArray[i][ii]=0;
+             }else if (array[i][ii] == 1 && neighbour ==8){
+                 bufferArray[i][ii]=0;
+             }else if (array[i][ii] == 0 && neighbour ==0){
+                 bufferArray[i][ii]=0;
+             }else if (array[i][ii] == 0 && neighbour ==1){
+                 bufferArray[i][ii]=0;
+             }else if (array[i][ii] == 0 && neighbour ==2){
+                 bufferArray[i][ii]=0;
+             }else if (array[i][ii] == 0 && neighbour ==3){
+                 bufferArray[i][ii]=1;
+             }else if (array[i][ii] == 0 && neighbour ==4){
+                 bufferArray[i][ii]=0;
+             }else if (array[i][ii] == 0 && neighbour ==5){
+                 bufferArray[i][ii]=0;
+             }else if (array[i][ii] == 0 && neighbour ==6){
+                 bufferArray[i][ii]=0;
+             }else if (array[i][ii] == 0 && neighbour ==7){
+                 bufferArray[i][ii]=0;
+             }else if (array[i][ii] == 0 && neighbour ==8){
+                 bufferArray[i][ii]=0;
+             }
+         }
+     }
+         
+ 
+     array = bufferArray
+ 
+ 
+     
+     
+     bufferArray = arrayZero();
+     
+     
+ }
 
-
-
-let intervalId = window.setInterval(function(){
-   animate()
- }, 10);
-
-
+ 
+ // Gets the relevant location from a mouse or single touch event
+ function getEventLocation(e)
+ {
+     if (e.touches && e.touches.length == 1)
+     {
+         return { x:e.touches[0].clientX, y: e.touches[0].clientY }
+     }
+     else if (e.clientX && e.clientY)
+     {
+         return { x: e.clientX, y: e.clientY }        
+     }
+ }
+ 
+ function drawRect(x, y, width, height)
+ {
+     ctx.fillRect( x, y, width, height )
+ }
+ 
+ function drawText(text, x, y, size, font)
+ {
+     ctx.font = `${size}px ${font}`
+     ctx.fillText(text, x, y)
+ }
+ 
+ let isDragging = false
+ let dragStart = { x: 0, y: 0 }
+ 
+ function onPointerDown(e)
+ {
+     isDragging = true
+     dragStart.x = getEventLocation(e).x/cameraZoom - cameraOffset.x
+     dragStart.y = getEventLocation(e).y/cameraZoom - cameraOffset.y
+ }
+ 
+ function onPointerUp(e)
+ {
+     isDragging = false
+     initialPinchDistance = null
+     lastZoom = cameraZoom
+ }
+ 
+ function onPointerMove(e)
+ {
+     if (isDragging)
+     {
+         cameraOffset.x = getEventLocation(e).x/cameraZoom - dragStart.x
+         cameraOffset.y = getEventLocation(e).y/cameraZoom - dragStart.y
+     }
+ }
+ 
+ function handleTouch(e, singleTouchHandler)
+ {
+     if ( e.touches.length == 1 )
+     {
+         singleTouchHandler(e)
+     }
+     else if (e.type == "touchmove" && e.touches.length == 2)
+     {
+         isDragging = false
+         handlePinch(e)
+     }
+ }
+ 
+ let initialPinchDistance = null
+ let lastZoom = cameraZoom
+ 
+ function handlePinch(e)
+ {
+     e.preventDefault()
+     
+     let touch1 = { x: e.touches[0].clientX, y: e.touches[0].clientY }
+     let touch2 = { x: e.touches[1].clientX, y: e.touches[1].clientY }
+     
+     // This is distance squared, but no need for an expensive sqrt as it's only used in ratio
+     let currentDistance = (touch1.x - touch2.x)**2 + (touch1.y - touch2.y)**2
+     
+     if (initialPinchDistance == null)
+     {
+         initialPinchDistance = currentDistance
+     }
+     else
+     {
+         adjustZoom( null, currentDistance/initialPinchDistance )
+     }
+ }
+ 
+ function adjustZoom(zoomAmount, zoomFactor)
+ {
+     if (!isDragging)
+     {
+         if (zoomAmount)
+         {
+             cameraZoom += zoomAmount
+         }
+         else if (zoomFactor)
+         {
+             console.log(zoomFactor)
+             cameraZoom = zoomFactor*lastZoom
+         }
+         
+         cameraZoom = Math.min( cameraZoom, MAX_ZOOM )
+         cameraZoom = Math.max( cameraZoom, MIN_ZOOM )
+         
+         console.log(zoomAmount)
+     }
+ }
+ 
+ canvas.addEventListener('mousedown', onPointerDown)
+ canvas.addEventListener('touchstart', (e) => handleTouch(e, onPointerDown))
+ canvas.addEventListener('mouseup', onPointerUp)
+ canvas.addEventListener('touchend',  (e) => handleTouch(e, onPointerUp))
+ canvas.addEventListener('mousemove', onPointerMove)
+ canvas.addEventListener('touchmove', (e) => handleTouch(e, onPointerMove))
+ canvas.addEventListener( 'wheel', (e) => adjustZoom(e.deltaY*SCROLL_SENSITIVITY))
+ 
+ // Ready, set, go
+ let intervalId = window.setInterval(function(){
+    draw()
+    }, 100);
